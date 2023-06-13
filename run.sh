736892897d5abe2b4bd9da26915446cfe6f647ef
bfren <bf@bfren.dev>
2023-06-13 10:22:59 +0100

    v1.3.0 (#34)
    Major updates
    * Adding Debian 12.0 'bookworm' release (as 'latest') and 13 'trixie'
    * Using latest base images
    
    Development updates
    * Using /bin/sh instead of bash
    * Run Debian 12 by default
    * Run ash not bash

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 IMAGE=`cat VERSION`
-DEBIAN=${1:-11}
+DEBIAN=${1:-12}
 
 docker buildx build \
     --build-arg BF_IMAGE=debian-s6 \
@@ -10,4 +10,4 @@ docker buildx build \
     -t debian${DEBIAN}-s6-dev \
     . \
     && \
-    docker run -it debian${DEBIAN}-s6-dev bash
+    docker run -it debian${DEBIAN}-s6-dev sh
