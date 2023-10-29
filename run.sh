3dc9e4c2d9d1a654892f4ad67c1b403095e8e2a0
bfren <bf@bfren.dev>
2023-10-29 15:35:59 +0000

    Adding BF_DEBUG=1

--- run.sh
+++ run.sh
@@ -5,10 +5,11 @@ DEBIAN=${1:-12}
 
 docker buildx build \
     --load \
+    --progress plain \
     --build-arg BF_IMAGE=debian-s6 \
     --build-arg BF_VERSION=${IMAGE} \
     -f ${DEBIAN}/Dockerfile \
     -t debian${DEBIAN}-s6-dev \
     . \
     && \
-    docker run -it debian${DEBIAN}-s6-dev sh
+    docker run -it -e BF_DEBUG=1 debian${DEBIAN}-s6-dev sh
