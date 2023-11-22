c7c6eec26799962f388835dd2e2bb40295f3a4c4
bfren <bf@bfren.dev>
2023-11-22 09:06:18 +0000

    v2.0.0 (#44)
    **Complete rewrite using [nushell](https://nushell.sh)**
    
    ## Major updates and breaking changes
    * Removing unsupported Debian versions
    * To enter nushell do `docker exec -it container_name nu` and type `bf-s6 + tab` to see a complete list of functions
    * `init.d` files must now be nu scripts, i.e. ending in `*.nu` or they will not run
    
    ## Minor updates
    * Updating to S6 Overlay 3.1.6.2
    
    ## Development updates
    * Forcing LF line endings

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,11 +4,11 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_REVISION="2.0.6"
-S6_VERSION="3.1.5.0"
-echo "Base: ${BASE_REVISION}"
+BASE_VERSION="3.0.0"
+S6_VERSION="3.1.6.2"
+ALPINE_BRANCH="v5.0.0"
 
-DEBIAN_VERSIONS="10 11 12 13"
+DEBIAN_VERSIONS="11 12 13"
 for V in ${DEBIAN_VERSIONS} ; do
 
     echo "Debian ${V}"
@@ -19,7 +19,8 @@ for V in ${DEBIAN_VERSIONS} ; do
         -e BF_DEBUG=0 \
         bfren/alpine esh \
         "/ws/Dockerfile.esh" \
-        BASE_REVISION=${BASE_REVISION} \
+        ALPINE_BRANCH=${ALPINE_BRANCH} \
+        BASE_VERSION=${BASE_VERSION} \
         S6_VERSION=${S6_VERSION} \
         DEBIAN_NAME=${DEBIAN_NAME}
     )
