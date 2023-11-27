14cb2107bef72cf778effad789f3cc40d2209d36
bfren <bf@bfren.dev>
2023-11-27 09:46:04 +0000

    v2.0.4 (#48)
    Minor updates
    * Using latest base images and Alpine S6 overlay

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_VERSION="3.0.3"
-ALPINE_BRANCH="v5.0.3"
+BASE_VERSION="3.0.4"
+ALPINE_BRANCH="v5.0.4"
 S6_VERSION="3.1.6.2"
 
 DEBIAN_VERSIONS="11 12 13"
