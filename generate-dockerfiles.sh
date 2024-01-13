6cb7810aea9bf0ba13fa61db714461a3a327951a
bfren <bf@bfren.dev>
2024-01-13 10:58:54 +0000

    v2.0.7 (#51)
    Minor updates
    * Using latest base images
    
    Documentation updates
    * Updating copyright year to 2024

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_VERSION="3.0.7"
-ALPINE_BRANCH="v5.1.1"
+BASE_VERSION="3.0.8"
+ALPINE_BRANCH="v5.1.2"
 S6_VERSION="3.1.6.2"
 
 DEBIAN_VERSIONS="11 12 13"
