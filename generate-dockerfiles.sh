03b9b3d76a83430aac295c59f9d3fc17e1b1d9a0
bfren <bf@bfren.dev>
2024-02-23 18:29:42 +0000

    v2.1.0 (#55)
    Major updates
    * Adding new test framework
    
    Minor updates
    * Using latest base image
    * Using latest Alpine S6 overlay

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_VERSION="3.0.11"
-ALPINE_BRANCH="v5.1.4"
+BASE_VERSION="3.1.0"
+ALPINE_BRANCH="v5.2.2"
 S6_VERSION="3.1.6.2"
 
 DEBIAN_VERSIONS="11 12 13"
---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+IMAGE=debian-s6
+VERSION=`cat VERSION`
+DEBIAN=${1:-12}
+TAG=${IMAGE}-test
+
+docker buildx build \
+    --load \
+    --build-arg BF_IMAGE=${IMAGE} \
+    --build-arg BF_VERSION=${VERSION} \
+    -f ${DEBIAN}/Dockerfile \
+    -t ${TAG} \
+    . \
+    && \
+    docker run --entrypoint "/usr/bin/env" ${TAG} -i nu -c "use bf test ; test"
