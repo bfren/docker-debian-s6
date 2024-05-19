66efa0624f86e5dadb9195def3fe0e8efc1d77c0
bfren <bf@bfren.dev>
2024-05-19 13:01:54 +0100

    Using latest base images and Alpine branch

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_VERSION="3.1.3"
-ALPINE_BRANCH="v5.2.5"
+BASE_VERSION="3.1.4"
+ALPINE_BRANCH="v5.2.6"
 S6_VERSION="3.1.6.2"
 
 DEBIAN_VERSIONS="11 12 13"
